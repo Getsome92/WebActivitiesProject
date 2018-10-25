8e1dad42224919729e0cfc54d2cb54d8bf7c7613
Getsome <pawel.d.kolendo@gmail.com>
2018-10-25 16:32:52 +0200

    Added User Creation/Comments/MongoDB support

--- app.js
+++ app.js
@@ -1,31 +1,61 @@
 var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
-    mongoose = require("mongoose")
-    Activity = require("./models/activity")
-    seedDB   = require("./seeds")
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    Activity = require("./models/activity"),
+    Comment = require("./models/comment"),
+    User = require("./models/user"),
+    seedDB = require("./seeds")
 
 
-mongoose.connect("mongodb://localhost:27017/activities", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/activities", {
+    useNewUrlParser: true
+});
 seedDB();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+// PASSPORT
+
+app.use(require("express-session")({
+    secret: "This is the hashing",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
 
 app.get("/", function (req, res) {
     res.render("homepage");
 });
 
-app.get("/activities", function (req, res){
-            //Get all the activities
-            Activity.find({}, function(err, activities){
-                if(err){
-                    console.log(err);
-                } else {
-                    res.render("index", {activities: activities});
-                }
+app.get("/activities", function (req, res) {
+    //Get all the activities
+    Activity.find({}, function (err, activities) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("activities/index", {
+                activities: activities
             });
+        }
+    });
 
-        });
+});
 
 app.post("/activities", function (req, res) {
     var name = req.body.name;
@@ -49,24 +79,103 @@ app.post("/activities", function (req, res) {
 });
 
 app.get("/activities/new", function (req, res) {
-    res.render("new.ejs");
+    res.render("activities/new");
 });
 //app.listen(process.env.PORT, process.env.IP,  function(){
 // console.log("connected")
 // })
 
-app.get("/activities/:id", function(req,res){
+app.get("/activities/:id", function (req, res) {
 
-    Activity.findById(req.params.id).populate("comments").exec(function(err, foundActivity){
-        if(err){
+    Activity.findById(req.params.id).populate("comments").exec(function (err, foundActivity) {
+        if (err) {
             console.log(err)
         } else {
             console.log(foundActivity);
-            res.render("show", {activities: foundActivity});
+            res.render("activities/show", {
+                activities: foundActivity
+            });
         }
     });
 });
 
+//========================
+// COMMENT SECTION
+//========================
+
+app.get("/activities/:id/comments/new",isLoggedin, function (req, res) {
+    Activity.findById(req.params.id, function (err, activity) {
+        if (err) {
+            console.log(err);
+
+        } else {
+            res.render("comments/new", {
+                activities: activity
+            });
+        }
+    });
+});
+
+app.post("/activities/:id/comments", isLoggedin, function (req, res) {
+    Activity.findById(req.params.id, function (err, activity) {
+        if (err) {
+            console.log(err);
+            redirect("/activities");
+        } else {
+            Comment.create(req.body.comment, function (err, comment) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    activity.comments.push(comment);
+                    activity.save();
+                    res.redirect("/activities/" + activity._id);
+                }
+            });
+        }
+    });
+});
+
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+app.post("/register", function (req, res) {
+    var newUser = new User({
+        username: req.body.username
+    });
+    User.register(newUser, req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function () {
+            res.redirect("/activities");
+        });
+    });
+});
+
+app.get("/login", function (req, res) {
+    res.render("login");
+})
+
+app.post("/login", passport.authenticate("local", {
+        successRedirect: "/activities",
+        failureRedirect: "/login"
+    }),
+    function (req, res) {
+
+    });
+app.get("/logout", function(req,res){
+    req.logout();
+    res.redirect("/activities");
+});
+
+function isLoggedin(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+};
 
 var port = process.env.PORT || 3000;
 var ip = process.env.IP || "localhost";
--- seeds.js
+++ seeds.js
@@ -5,15 +5,15 @@ var Comment = require("./models/comment");
 var data = [{
     name: "Gokarts",
     image: "https://cdn.pixabay.com/photo/2016/10/20/03/00/kart-1754533__340.jpg",
-    describtion: "Twoja matka chcialaby to robic"
+    describtion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 }, {
     name: "Samochodziki",
     image: "https://cdn.pixabay.com/photo/2016/10/20/03/00/kart-1754533__340.jpg",
-    describtion: "Twoja matka chcialaby to robic"
+    describtion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 }, {
     name: "Pojazdy",
     image: "https://cdn.pixabay.com/photo/2016/10/20/03/00/kart-1754533__340.jpg",
-    describtion: "Twoja matka chcialaby to robic"
+    describtion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 }]
 
 function seedDB() {
